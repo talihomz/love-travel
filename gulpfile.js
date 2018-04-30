9bc1a380044f67b620f8ab0c3f4a6d2688a33746
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-04-30 14:18:55 +0200

    intro section layout created

--- gulpfile.js
+++ gulpfile.js
@@ -2,46 +2,54 @@ const gulp        = require('gulp');
 const browserSync = require('browser-sync').create();
 const runSequence = require('run-sequence');
 const sass        = require('gulp-sass');
-const imagemin    = require('gulp-imagemin');
+const image       = require('gulp-imagemin');
 
 // Static Server + watching scss/html files
-gulp.task('serve', function() {
+gulp.task('serve', () => {
 
   browserSync.init({
       server: "public"
   });
 
-  gulp.watch("src/home.html", ['html']);
+  gulp.watch("src/js/*.js", ['js']);
+  gulp.watch("src/index.html", ['html']);
   gulp.watch("src/scss/**/*.scss", ['sass']);
   gulp.watch("public/*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-  return gulp.src("src/scss/main.scss")
+gulp.task('sass', () =>
+  gulp.src("src/scss/main.scss")
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest("public/css"))
-    .pipe(browserSync.stream());
-});
-
-// Compress images
-gulp.task('images', () =>
-    gulp.src('src/images/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('public/images'))
+    .pipe(browserSync.stream())
 );
 
+// Compress images
+gulp.task('images', () => {
+  return gulp.src('src/images/*')
+    .pipe(image())
+    .pipe(gulp.dest('public/images'))
+});
+
 // Browser Sync
-gulp.task('html', function(){
+gulp.task('html', () => {
   gulp.src('src/index.html')
     .pipe(gulp.dest('public'))
-    .pipe(browserSync.stream());
+    .pipe(browserSync.stream())
+});
+
+// Compile Javascript
+gulp.task('js', () => {
+  gulp.src('src/js/*.js')
+    .pipe(gulp.dest('public/js'))
+    .pipe(browserSync.stream())
 });
 
 // Default task
-gulp.task('default', function(callback){
+gulp.task('default', (callback) =>
   runSequence('sass',
-              ['images', 'html'],
+              ['images', 'html', 'js'],
               'serve',
-              callback);
-});
+              callback)
+);
